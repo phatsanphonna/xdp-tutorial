9534ee7eb16d12ae49c326b09dc36e1db471fda8
phatsanphon2004@outlook.com <phatsanphon2004@outlook.com>
2024-12-03 08:31:51 +0000

    drop ipv4 packet

--- packet01-parsing/xdp_prog_kern.c
+++ packet01-parsing/xdp_prog_kern.c
@@ -5,6 +5,8 @@
 #include <linux/if_ether.h>
 #include <linux/if_packet.h>
 #include <linux/ipv6.h>
+#include <linux/ip.h>
+#include <linux/icmp.h>
 #include <linux/icmpv6.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
@@ -78,6 +80,36 @@ static __always_inline int parse_icmp6hdr(struct hdr_cursor *nh,
 	return icmp6h->icmp6_type;
 }
 
+static __always_inline int parse_ip4hdr(struct hdr_cursor *nh,
+										void *data_end,
+										struct iphdr **iphdr)
+{
+	struct iphdr *iph = nh->pos;
+
+	if (iph + 1 > data_end)
+		return -1;
+
+	nh->pos = iph + 1;
+	*iphdr = iph;
+
+	return iph->protocol;
+}
+
+static __always_inline int parse_icmp4hdr(struct hdr_cursor *nh,
+										  void *data_end,
+										  struct icmphdr **icmphdr)
+{
+	struct icmphdr *icmph = nh->pos;
+
+	if (icmph + 1 > data_end)
+		return -1;
+
+	nh->pos = icmph + 1;
+	*icmphdr = icmph;
+
+	return icmph->type;
+}
+
 SEC("xdp")
 int xdp_parser_func(struct xdp_md *ctx)
 {
@@ -103,31 +135,57 @@ int xdp_parser_func(struct xdp_md *ctx)
 	 * header type in the packet correct?), and bounds checking.
 	 */
 	nh_type = parse_ethhdr(&nh, data_end, &eth);
-	if (nh_type != bpf_htons(ETH_P_IPV6))
-		goto out;
-
-	/* Assignment additions go below here */
-	struct ipv6hdr *ip6h;
-	nh_type = parse_ip6hdr(&nh, data_end, &ip6h);
-	if (nh_type != IPPROTO_ICMPV6)
-		goto out;
-
-	struct icmp6hdr *icmp6h;
-	int icmp_type = parse_icmp6hdr(&nh, data_end, &icmp6h);
-	if (icmp_type != ICMPV6_ECHO_REQUEST)
-		goto out;
-
-	bpf_printk("ICMPv6 sequence: %u", bpf_ntohs(icmp6h->icmp6_dataun.u_echo.sequence));
-	for (int i = 0; i < sizeof(ip6h->daddr.in6_u.u6_addr8) / sizeof(unsigned char); i += 2)
+	bpf_printk("ETH Type: %u %u %u", nh_type, bpf_htons(ETH_P_IP), bpf_htons(ETH_P_IPV6));
+	if (nh_type == bpf_htons(ETH_P_IPV6))
 	{
-		unsigned char front = ip6h->daddr.in6_u.u6_addr8[i];
-		unsigned char back = ip6h->daddr.in6_u.u6_addr8[i + 1];
-	
-		bpf_printk("%02x%02x", front, back);
+		struct ipv6hdr *ip6h;
+		nh_type = parse_ip6hdr(&nh, data_end, &ip6h);
+		if (nh_type != IPPROTO_ICMPV6)
+			goto out;
+
+		struct icmp6hdr *icmp6h;
+		int icmp_type = parse_icmp6hdr(&nh, data_end, &icmp6h);
+		if (icmp_type != ICMPV6_ECHO_REQUEST)
+			goto out;
+
+		bpf_printk("ICMPv6 sequence: %u", bpf_ntohs(icmp6h->icmp6_dataun.u_echo.sequence));
+		for (int i = 0; i < sizeof(ip6h->daddr.in6_u.u6_addr8) / sizeof(unsigned char); i += 2)
+		{
+			unsigned char front = ip6h->daddr.in6_u.u6_addr8[i];
+			unsigned char back = ip6h->daddr.in6_u.u6_addr8[i + 1];
+
+			bpf_printk("%02x%02x", front, back);
+		}
+
+		if (bpf_ntohs(icmp6h->icmp6_sequence) % 2 == 0)
+			action = XDP_DROP;
+	}
+	else if (nh_type == bpf_htons(ETH_P_IP))
+	{
+		/* Assignment additions go below here */
+		struct iphdr *iph;
+		nh_type = parse_ip4hdr(&nh, data_end, &iph);
+		bpf_printk("IP Type: %u %u", nh_type, IPPROTO_ICMP);
+
+		if (nh_type != IPPROTO_ICMP)
+			goto out;
+
+		struct icmphdr *icmph;
+		int icmp_type = parse_icmp4hdr(&nh, data_end, &icmph);
+		if (icmp_type != ICMP_ECHO)
+			goto out;
+
+		bpf_printk("ICMPv6 sequence: %u", bpf_ntohs(icmph->un.echo.sequence));
+		bpf_printk("IPv4 Address: %d.%d.%d.%d",
+           (iph->daddr & 0xFF),
+           (iph->daddr >> 8) & 0xFF,
+           (iph->daddr >> 16) & 0xFF,
+           (iph->daddr >> 24) & 0xFF);
+
+		if (bpf_ntohs(icmph->un.echo.sequence) % 2 == 0)
+			action = XDP_DROP;
 	}
 
-	if (bpf_ntohs(icmp6h->icmp6_sequence) % 2 == 0)
-		action = XDP_DROP;
 out:
 	return xdp_stats_record_action(ctx, action); /* read via xdp_stats */
 }
